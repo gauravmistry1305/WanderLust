c177d91ef1816533b64902cac1dd8818442900ab
gauravmistry1305 <mistrypc29@gmail.com>
2025-02-13 23:15:32 +0530

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
