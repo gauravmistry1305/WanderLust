59de7bb912f6448e989e32d06bb3518f4cf29146
gauravmistry1305 <mistrypc29@gmail.com>
2025-02-19 13:16:01 +0530

    commit

--- src/components/MD.jsx
+++ src/components/MD.jsx
@@ -2,8 +2,9 @@ import './MD.css';
 
 const MD = () => {
   return (
-    <div>
-      
+    <div className='md'>
+     <h1> hello,</h1>
+      <p>here&apos;s the more-destinations</p>
     </div>
   )
 }
