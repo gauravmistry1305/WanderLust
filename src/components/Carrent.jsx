488c6fa48daf436efa48286f1e4fb5eb240d7d33
gauravmistry1305 <mistrypc29@gmail.com>
2025-02-26 16:55:15 +0530

    commit

--- src/components/Carrent.jsx
+++ src/components/Carrent.jsx
@@ -1,12 +1,33 @@
-import './Carrent.css';
+import "./Carrent.css";
 import CarNav from './CarNav';
 
 const Carrent = () => {
   return (
-    <div>
-      <CarNav/>
+    <div className="bg-gray-100">     
+         <CarNav/>
+      <main>
+        <section className="hero">
+          <img
+            src="./src/img/carimg2.jpg"
+            alt="Red car on a scenic road"
+            className="hero-image"
+          />
+          <div className="hero-content">
+            <h1>FIND THE IDEAL CAR FOR YOU.</h1>
+            <p>We have more cars for you to choose.</p>
+          </div>
+        </section>
+        <section className="info-section">
+          <h2>Find the Best Car For You</h2>
+          <p>
+            It is above all the uncompromising, performance-oriented aesthetic
+            that unmistakably reveals its ambitions. Not elegant but
+            extravagant. Not conventional but individual.
+          </p>
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Carrent
+export default Carrent;
